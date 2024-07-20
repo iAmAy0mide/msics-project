e0542d9843fcd141c72a77d8ad44561df7d5164e
iAmAy0mide <ayomidekenny952@gmail.com>
2024-07-20 13:19:31 -0700

    ups

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='fixed h-16 w-full mx-auto px-4 sm:px-8 md:px-12'>
+        <div className=" w-full h-full bg-slate-400">
+
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar;
