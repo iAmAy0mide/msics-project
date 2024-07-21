748417ef9f2f1f7db9effa52832a39c44ebb59c0
iAmAy0mide <ayomidekenny952@gmail.com>
2024-07-21 02:13:50 -0700

    ups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Mush from "/public/mushroom.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+    <main className="">
+      <section className="grid sm:grid-cols-2 place-items-center pt-24">
+        <div className="">
+          <p className="">Psilocybin pioneers</p>
+          <h1 className="">Developing & providing innovative naturally sourced GMP psilocybin.</h1>
+        </div>
+        <div className="max-w-[640px] w-[640px]">
+          <Image 
+            className="w-full"
+            src={Mush}
+            width={100}
+            height={100}
+            alt=""
+          />
+        </div>
+      </section>
+      
     </main>
   );
 }
