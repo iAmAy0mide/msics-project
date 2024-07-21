7936686869b47fad8135531b8e568ba36e864d76
iAmAy0mide <ayomidekenny952@gmail.com>
2024-07-21 04:45:08 -0700

    partner section cplet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import ManOnLabBed from "/public/man-on-lab-bed.svg"
 import ManinLabd from "/public/man-in-lab.svg";
 import Psilocybin from "/public/processed-psilocybin.svg";
 import PhialBottle from "/public/phial-bottle.svg";
+import Logo from "/public/Logo.svg";
+import GrayBg from "/public/gray-bg.png";
+import GrainTexture from "/public/grains-texture.png";
 import { Livvic } from "next/font/google";
 
 const livvic = Livvic({
@@ -13,8 +16,8 @@ const livvic = Livvic({
 
 export default function Home() {
   return (
-    <main className="pt-24 mobile px-8 md:px-12 mx-auto">
-      <section className="grid sm:grid-cols-2 items-center">
+    <main className="pt-24 ">
+      <section className="grid sm:grid-cols-2 items-center mobile px-8 md:px-12 mx-auto">
         <div className="">
           <p className="font-medium text-3xl mb-8">Psilocybin pioneers</p>
           <h1 className={`${livvic.className} font-medium text-[50px] leading-[3rem] -ml-1`}>Developing & providing innovative naturally sourced GMP psilocybin.</h1>
@@ -30,15 +33,17 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="mission" className="w-full my-16 h-[700px] mx-auto rounded-[2rem]" style={{ backgroundImage: `url(${ManOnLabBed.src})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
-        <div className="w-full place-items-center flex flex-col space-y-6 pt-8 text-[#fff]">
-          <p className="font-medium text-md uppercase">A new era of medicine</p>
-          <h2 className={` ${livvic.className} font-medium text-[2.5rem] `}>MSICS&apos;s mission</h2>
-          <p className="w-[70%] text-pretty text-center">To revolutionize global mental healthcare, we harness the power of naturally sourced psilocybin, creating innovative products to effectively address the world&apos;s health challenges.</p>
+      <section className="mobile px-8 md:px-12 mx-auto h-[700px] my-16 ">
+        <div id="mission" className="w-full h-full  rounded-[2rem]" style={{ backgroundImage: `url(${ManOnLabBed.src})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+          <div className="w-full place-items-center flex flex-col space-y-6 pt-8 text-[#fff]">
+            <p className="font-medium uppercase">A new era of medicine</p>
+            <h2 className={` ${livvic.className} font-medium text-[2.5rem] `}>MSICS&apos;s mission</h2>
+            <p className="w-[70%] text-pretty text-center">To revolutionize global mental healthcare, we harness the power of naturally sourced psilocybin, creating innovative products to effectively address the world&apos;s health challenges.</p>
+          </div>
         </div>
       </section>
 
-      <section id="product" className="">
+      <section id="product" className="mobile px-8 md:px-12 mx-auto" >
         <div className="grid md:grid-cols-2 items-center">
           <div className="">
             <p className="uppercase font-medium text-3xl mb-8">MSICS&apos;S PRODUCTS</p>
@@ -75,6 +80,39 @@ export default function Home() {
                 />
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact" className="glassy-white my-16 p-10" style={{ backgroundImage: `url(${GrainTexture.src})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+        <h2 className={` ${livvic.className} font-medium text-[2.3rem] text-center`}>Our Partners</h2>
+        <div className="flex w-full justify-between items-center mt-10">
+          <div className="">
+            <Image 
+              width={100}
+              height={100}
+              src={Logo}
+              alt="Logo for pharma"
+              className="w-full"
+            />
+          </div>
+          <div className="">
+            <Image 
+              width={100}
+              height={100}
+              src={Logo}
+              alt="Logo for pharma"
+              className="w-full"
+            />
+          </div>
+          <div className="">
+            <Image 
+              width={100}
+              height={100}
+              src={Logo}
+              alt="Logo for pharma"
+              className="w-full"
+            />
           </div>
         </div>
       </section>
