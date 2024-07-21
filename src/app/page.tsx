5f399e27ff7231b05242169c1e3dca52441073ed
iAmAy0mide <ayomidekenny952@gmail.com>
2024-07-21 03:22:02 -0700

    mission section complte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
 import Image from "next/image";
-import Mush from "/public/mushroom.svg";
+import mushroom from "/public/mushroom.svg";
+import ManOnLabBed from "/public/man-on-lab-bed.svg"
+import { Livvic } from "next/font/google";
+
+const livvic = Livvic({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"]
+});
 
 export default function Home() {
   return (
-    <main className="">
-      <section className="grid sm:grid-cols-2 place-items-center pt-24">
+    <main className="pt-24 mobile px-8 md:px-12 mx-auto">
+      <section className="grid sm:grid-cols-2 items-center">
         <div className="">
-          <p className="">Psilocybin pioneers</p>
-          <h1 className="">Developing & providing innovative naturally sourced GMP psilocybin.</h1>
+          <p className="font-medium text-3xl mb-8">Psilocybin pioneers</p>
+          <h1 className={`${livvic.className} font-medium text-[50px] leading-[3rem] -ml-1`}>Developing & providing innovative naturally sourced GMP psilocybin.</h1>
         </div>
-        <div className="max-w-[640px] w-[640px]">
+        <div className="md:max-w-[640px] md:w-[635px]">
           <Image 
-            className="w-full"
-            src={Mush}
+            className="w-full rounded-[30px]"
+            src={mushroom}
             width={100}
             height={100}
-            alt=""
+            alt="mushroom from which psilocybin was made"
           />
+        </div>
+      </section>
+      <section className="w-full my-16 h-[700px] mx-auto rounded-[30px]" style={{ backgroundImage: `url(${ManOnLabBed.src})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+        <div className="w-full place-items-center flex flex-col space-y-6 pt-8 text-[#fff]">
+          <p className="font-medium text-md uppercase">A new era of medicine</p>
+          <h2 className={` ${livvic.className} font-medium text-[40px] `}>MSICS&apos;s mission</h2>
+          <p className="w-[70%] text-pretty text-center">To revolutionize global mental healthcare, we harness the power of naturally sourced psilocybin, creating innovative products to effectively address the world&apos;s health challenges.</p>
         </div>
       </section>
       
