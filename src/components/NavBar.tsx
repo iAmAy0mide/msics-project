48258aeed9627737bd349f4f8c25d635fdf0431a
iAmAy0mide <ayomidekenny952@gmail.com>
2024-07-21 00:54:33 -0700

    added nav items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Logo from "/public/Logo.svg";
+import Image from "next/image";
+import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <nav className='fixed h-16 w-full mx-auto px-4 sm:px-8 md:px-12'>
-        <div className=" w-full h-full bg-slate-400">
-
+    <nav className='fixed pt-4 flex items-center justify-between h-16 w-full mx-auto mobile px-8 md:px-12'>
+        <Link href="/" className='w-40'>
+          <Image 
+           className='w-full'
+           width={100} 
+           height={100}
+           alt='Logo'
+           src={Logo}
+          />
+        </Link>
+        <div className="flex space-x-16">
+          <Link 
+            href="#mission"
+            className=''
+             >
+              Mission
+          </Link>
+          <Link 
+            href="#product"
+            className=''
+             >
+              Product
+          </Link>
+          <Link 
+            href="#contact"
+            className=''
+             >
+              Contact
+          </Link>
         </div>
     </nav>
   )
